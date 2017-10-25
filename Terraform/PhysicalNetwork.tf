d64c1b3fb781033344200a9d199b8174690e4ea2
johnstudarus <john@jhlconsulting.com>
2017-10-25 18:12:49 +0000

    5 second timeout ignore ping output while busy waiting for system to reboot

--- Terraform/PhysicalNetwork.tf
+++ Terraform/PhysicalNetwork.tf
@@ -27,9 +27,9 @@ resource "null_resource" "physical_network" {
     command = "sleep 60"
   }
 
-  # wait until port 22 (SSH) comes back online
+  # wait until port 22 (SSH) comes back online first number is timeout
   provisioner "local-exec" {
-    command = "until nc -vzw 22 ${element(packet_device.controller.*.access_public_ipv4, count.index)} 22; do sleep 2; done"
+    command = "until nc -vzw 5 ${element(packet_device.controller.*.access_public_ipv4, count.index)} 22 2> /dev/null; do sleep 2; done"
   }
 
   provisioner "file" {
