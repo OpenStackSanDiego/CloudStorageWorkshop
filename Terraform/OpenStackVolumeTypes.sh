d915b2956a8f1b5233f36274a5f772c246d35ebc
johnstudarus <john@jhlconsulting.com>
2017-10-27 13:05:17 -0700

    Update OpenStackVolumeTypes.sh

--- Terraform/OpenStackVolumeTypes.sh
+++ Terraform/OpenStackVolumeTypes.sh
@@ -1,26 +1,15 @@
 source ~/keystonerc_admin
 
 openstack volume type create --public \
-	--description "QOS: XYZ. $/GB/Month: $6" \
+	--description "Min: 1000 IOPs, Max: 5000 IOPs, Burst: 10000 IOPs" \
 	Platinum
-#openstack volume type create --public \
-	#--encryption-cipher LUKS \
-	#--description "QOS: XYZ. $/GB/Month: $7" \
-	#Platinum-Encrypted
 openstack volume type create --public \
-	--description "QOS: XYZ. $/GB/Month: $4" \
+	--description "Min: 500 IOPs, Max: 2500 IOPs, Burst: 5000 IOPs" \
 	Gold
-#openstack volume type create --public \
-	#--encryption-cipher LUKS \
-	#--description "QOS: XYZ. $/GB/Month: $5" \
-	#Gold-Encrypted
+cinder encryption-type-create Gold LUKS
 openstack volume type create --public \
-	--description "QOS: XYZ. $/GB/Month: $2" \
+	--description "Min: 100 IOPs, Max: 500 IOPs, Burst: 1000 IOPs" \
 	Silver
-#openstack volume type create --public \
-	#--encryption-cipher LUKS \
-	#--description "QOS: XYZ. $/GB/Month: $3" \
-	#Silver-Encrypted
 openstack volume type create --public \
-	--description "QOS: XYZ. $/GB/Month: $1" \
+	--description "Min: 0 IOPs, Max: 100 IOPs, Burst: 500 IOPs" \
 	Bronze
