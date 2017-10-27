bb1a852de6a2a0318a58355bb26adbeec9ee874d
johnstudarus <john@jhlconsulting.com>
2017-10-27 13:11:34 -0700

    Update OpenStackVolumeTypes.sh

--- Terraform/OpenStackVolumeTypes.sh
+++ Terraform/OpenStackVolumeTypes.sh
@@ -1,15 +1,15 @@
 source ~/keystonerc_admin
 
 openstack volume type create --public \
-	--description "Min: 1000 IOPs, Max: 5000 IOPs, Burst: 10000 IOPs" \
+	--description "Min: 1000 IOPS, Max: 5000 IOPS, Burst: 10000 IOPS $10/GB/month" \
 	Platinum
 openstack volume type create --public \
-	--description "Min: 500 IOPs, Max: 2500 IOPs, Burst: 5000 IOPs" \
+	--description "Min: 500 IOPS, Max: 2500 IOPS, Burst: 5000 IOPS $5/GB/month" \
 	Gold
 cinder encryption-type-create Gold LUKS
 openstack volume type create --public \
-	--description "Min: 100 IOPs, Max: 500 IOPs, Burst: 1000 IOPs" \
+	--description "Min: 100 IOPS, Max: 500 IOPS, Burst: 1000 IOPS $3/GB/month" \
 	Silver
 openstack volume type create --public \
-	--description "Min: 0 IOPs, Max: 100 IOPs, Burst: 500 IOPs" \
+	--description "Min: 0 IOPS, Max: 100 IOPS, Burst: 500 IOPS $1/GB/month" \
 	Bronze
